a55a40574f82b38ba8aa3489fde34780329f8918
suaadsaid212-ux <suaadsaid212@gmail.com>
2026-06-04 18:01:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-page_title="Reputation Intelligence Platform",
-page_icon="🌍",
-layout="wide"
+    page_title="Reputation Intelligence Platform",
+    page_icon="🌍",
+    layout="wide",
 )
