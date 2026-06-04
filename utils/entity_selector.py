65f54fea632cff62e55a55ab824270f5ae8f0b15
suaadsaid212-ux <suaadsaid212@gmail.com>
2026-06-04 15:55:19 +0300

    Update entity_selector.py

--- utils/entity_selector.py
+++ utils/entity_selector.py
@@ -1,19 +1,25 @@
-import pandas as pd
 import streamlit as st
+import pandas as pd
 
-def get_selected_entity():
+@st.cache_data
+def load_registry():
 
-    df = pd.read_csv(
-        "config/entity_registry.csv"
+    return pd.read_csv(
+        "config/entity_registry.csv",
+        encoding="utf-8-sig"
     )
 
+def get_entity():
+
+    df = load_registry()
+
     selected = st.sidebar.selectbox(
-        "Select Entity",
-        df["Entity Name"]
+        "🏢 Select Entity",
+        df["Entity_Name"]
     )
 
     entity = df[
-        df["Entity Name"] == selected
+        df["Entity_Name"] == selected
     ].iloc[0]
 
     return entity
