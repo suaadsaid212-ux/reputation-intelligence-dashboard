209e95438fa18d18e73251a12cd2cef9736e79c7
suaadsaid212-ux <suaadsaid212@gmail.com>
2026-06-04 19:29:02 +0300

    Update scoring_utils.py

--- utils/scoring_utils.py
+++ utils/scoring_utils.py
@@ -1,13 +1,17 @@
 def calculate_rii(
     exposure,
     vulnerability,
-    resilience
+    resilience,
 ):
-
     rii = (
-        exposure +
-        vulnerability -
-        resilience
+        0.35 * exposure
+        + 0.35 * vulnerability
+        - 0.30 * resilience
+    )
+
+    rii = max(
+        0,
+        min(100, rii),
     )
 
     return round(rii, 2)
